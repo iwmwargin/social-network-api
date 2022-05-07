8e4d59ce9210ff0edfa418c313f04186eaafca40
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-05-07 11:52:54 -0400

    added User js code

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model, trusted } = require("mongoose");
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email address!"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+const User = model("User", UserSchema);
+
+UserSchema.virtual("friendCount").get(function() {
+    return this.friends.length
+});
+
+module.exports = User;
